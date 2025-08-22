609913da43622025c5b5c82ecee546a034bbeb6d
ezequiasherrera <ezequiasherrera99@gmail.com>
2025-08-22 10:37:08 -0400

    Fix in print that reboot Render

--- products/products_loader.py
+++ products/products_loader.py
@@ -18,8 +18,5 @@ def cargar_productos():
     with open(PRODUCTOS_FILE, "r", encoding="utf-8") as f:
         products_list = {p["codigo"]: p for p in json.load(f)}
 
-    print(f"{products_list[str(1141801)]["precio"]}")
-
-
     AppState.is_products_list_loaded = True
     print(f"📦 Productos cargados en memoria: {len(products_list)}")
