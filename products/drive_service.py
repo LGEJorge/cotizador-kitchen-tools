68b9c47f11bc1e6554a3d946a542cb0b9f44fc8f
ezequiasherrera <ezequiasherrera99@gmail.com>
2025-10-24 16:00:47 -0400

    Changing to the other Drive folder

--- products/drive_service.py
+++ products/drive_service.py
@@ -6,7 +6,7 @@ def crear_drive_service():
     return build("drive", "v3", credentials=credentials)
 
 def obtener_imagen_base64_por_sku(service, sku):
-    carpeta_id = "1NMgqDd8fzBQV1ShiUWl-waSxxPvsUAaM"
+    carpeta_id = "1-R_zY7rBbem5DmHclokxLZF-wYsdvjep"
     query = f"name = '{sku}.jpg' and '{carpeta_id}' in parents and mimeType contains 'image/' and trashed = false"
     resultados = service.files().list(q=query, fields="files(id)").execute()
     archivos = resultados.get("files", [])
